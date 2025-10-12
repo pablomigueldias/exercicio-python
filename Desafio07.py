28d76339538557cdb0d3a5f4992de1d74bb855ec
Pablo Ortiz <pablo.miguel.dias@gmail.com>
2025-10-12 18:23:48 -0300

    Desafio 07 feito

---
+++ Desafio07.py
@@ -0,0 +1,8 @@
+# Desenvolva um programa quer leia as duas notas de um aluno, calcule  e mostre a sua media
+
+nota1 = float(input('Qual é a primeira nota do aluno? '))
+nota2 = float(input('Qual é a segunda nota do aluno? '))
+
+media = (nota1 + nota2) / 2
+
+print(f'A media do aluno é {media}')
