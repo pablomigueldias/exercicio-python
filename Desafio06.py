f628c315ec1e576bdcf87781b21cea1b0b0da4a8
Pablo Ortiz <pablo.miguel.dias@gmail.com>
2025-10-12 18:19:19 -0300

    Desafio 06 feito

---
+++ Desafio06.py
@@ -0,0 +1,12 @@
+# Crie um algoritmo que leia um numero e mostre o seu dobro, triplo e raiz quadrada
+
+numero = int(input('Digite um numero inteiro: '))
+
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero**(1/2)
+
+print(f'Seu numero é {numero}')
+print(f'O dobro é {dobro}')
+print(f'O triplo é {triplo}')
+print(f'A raiz quadrada é {raiz}')
