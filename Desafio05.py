b8a95a3b7f0606c377effb8e07dabc8dede5dcff
Pablo Ortiz <pablo.miguel.dias@gmail.com>
2025-10-12 18:13:28 -0300

    Desafio 5 feito

---
+++ Desafio05.py
@@ -0,0 +1,7 @@
+# faça um programa que leia um numero inteiro e mostre na tela o seu sucessor e seu antecessor
+
+numero = int(input('Digite um numero: '))
+antecessor = numero - 1
+sucessor = numero + 1
+
+print(f'Seu numero é {numero} seu antecessor é {antecessor} e seu sucessor é {sucessor}')
